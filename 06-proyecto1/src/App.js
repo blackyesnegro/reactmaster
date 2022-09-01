57e3820cec145b4080d6fd03b3bc494fa4293b74
blackyesnegro <blackyesnegro@hotmail.com>
2022-09-01 14:45:35 +0200

    Proyecto init

--- 06-proyecto1/src/App.js
+++ 06-proyecto1/src/App.js
@@ -16,10 +16,10 @@ function App() {
         {/*Barra de navegación*/}
         <nav className="nav">
             <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Peliculas</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contacto</a></li>
+                <li><a href="/#">Inicio</a></li>
+                <li><a href="/#">Peliculas</a></li>
+                <li><a href="/#">Blog</a></li>
+                <li><a href="/#">Contacto</a></li>
             </ul>
         </nav>
 
