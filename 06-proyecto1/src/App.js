aa5440ab0fba2c39469a958e6678e6fbd78f6350
blackyesnegro <blackyesnegro@hotmail.com>
2022-09-07 21:16:18 +0200

    CReacion de componentes

--- 06-proyecto1/src/App.js
+++ 06-proyecto1/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { Buscador } from './components/Buscador';
+import { Crear } from './components/Crear';
+import { Listado } from './components/Listado';
+
 
 function App() {
   return (
-    <div classNameName="layout">
+    <div className="layout">
         {/*Cabecera*/}
         <header className="header">
             <div className="logo">
@@ -27,58 +31,15 @@ function App() {
         <section id="content" className="content">
 
             {/*aqui van las peliculas*/}
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+            <Listado />
 
         </section>
 
         {/*Barra lateral*/}
         <aside className="lateral">
-            <div className="search">
-                <h3 className="title">Buscador</h3>
-                <form>
-                    <input type="text" id="search_field" />
-                    <button id="search">Buscar</button>
-                </form>
-            </div>
+            <Buscador />
 
-            <div className="add">
-                <h3 className="title">Añadir pelicula</h3>
-                <form>
-                    <input type="text" id="title" placeholder="Titulo" />
-                    <textarea id="description" placeholder="Descripción"></textarea>
-                    <input type="submit" id="save" value="Guardar" />
-                </form>
-            </div>
+            <Crear />
         </aside>
 
         {/*Pie de página*/}
