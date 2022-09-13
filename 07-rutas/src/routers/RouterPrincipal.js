6266b112afe23cfe53dd8342cddd7804f159de92
blackyesnegro <blackyesnegro@hotmail.com>
2022-09-13 19:02:46 +0200

    React Router

---
+++ 07-rutas/src/routers/RouterPrincipal.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Routes, Route, Links, BrowserRouter} from 'react-router-dom'
+
+export const RouterPrincipal = () => {
+  return (
+    <div>RouterPrincipal</div>
+  )
+}
