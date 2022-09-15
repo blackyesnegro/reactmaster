3340d3974497c6d5930ab4d10e5ee0072a4c5a8e
blackyesnegro <blackyesnegro@hotmail.com>
2022-09-15 20:58:49 +0200

    Rutas y subrutas

--- 07-rutas/src/components/Persona.js
+++ 07-rutas/src/components/Persona.js
@@ -1,7 +1,42 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export const Persona = () => {
+
+  //Valores por defecto
+  // let {nombre, apellido} = useParams();
+
+  const {nombre, apellido} = useParams();
+  const navegar = useNavigate();
+
+  const enviar = (e) => {
+    e.preventDefault();
+
+    let nombre = e.target.nombre.value;
+    let apellido = e.target.apellido.value;
+    let url = `/persona/${nombre}/${apellido}`;
+
+    console.log(e.target.nombre.value);
+    if(nombre.lenght <= 0 && apellido.lenght <= 0){
+      navegar('/inicio');
+    }else if(nombre === 'contacto'){
+      navegar('/contacto')
+    }else{
+      navegar(url);
+    }
+
+
+  }
+
   return (
-    <div>Persona</div>
+    <div>
+      {!nombre ? <h1>Persona General</h1> : <h1>Persona: {nombre} {apellido}</h1>}
+
+      <form onSubmit={enviar}>
+        <input type="text" name="nombre" />  
+        <input type="text" name="apellido" />  
+        <input type="submit" name="enviar" value="enviar" />
+      </form>   
+    </div>
   )
 }
--- 07-rutas/src/routers/RouterPrincipal.js
+++ 07-rutas/src/routers/RouterPrincipal.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import {Routes, Route, NavLink, BrowserRouter} from 'react-router-dom'
+import {Routes, Route, NavLink, BrowserRouter, Navigate} from 'react-router-dom'
 import {Articulos} from '../components/Articulos';
 import {Inicio} from '../components/Inicio';
 import {Contacto} from '../components/Contacto';
 import {Error} from '../components/Error';
+import {Persona} from '../components/Persona';
 
 export const RouterPrincipal = () => {
   return (
@@ -25,6 +26,12 @@ export const RouterPrincipal = () => {
           </li>
           <li>
             <NavLink 
+              to="/persona"
+              className={({isActive}) => isActive ? 'activado' : ''}
+            >Persona</NavLink>
+          </li>
+          <li>
+            <NavLink 
               to="/contacto"
               className={({isActive}) => isActive ? 'activado' : ''}
             >Contacto</NavLink>
@@ -37,7 +44,11 @@ export const RouterPrincipal = () => {
           <Route path="/" element={<Inicio />} />
           <Route path="/inicio" element={<Inicio />} />
           <Route path="/articulos" element={<Articulos />} />
+          <Route path="/persona/:nombre/:apellido" element={<Persona />} />
+          <Route path="/persona/:nombre/" element={<Persona />} />
+          <Route path="/persona" element={<Persona />} />
           <Route path="/contacto" element={<Contacto />} />
+          <Route path="/redirigir" element={<Navigate to="/persona/JuanP/Amoros"/>} />
           <Route path="*" element={<Error />} />
         </Routes>
       </section>
